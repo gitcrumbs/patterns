33d82634828083d8e3cb934713e105de0b3da4fa
gitcrumbs <ashwanisingh034@gmail.com>
2023-02-04 02:25:22 +0530

    Chain of Responsibility Fixes

--- src/main/java/com/upgrad/patterns/Config/RestServiceGenerator.java
+++ src/main/java/com/upgrad/patterns/Config/RestServiceGenerator.java
@@ -19,17 +19,20 @@ public class RestServiceGenerator {
     public static RestTemplate GetInstance()
     {
         // return restTemplate object if initialized already
-    	
 
-        // Initialize restTemplate. This is executed only once.
-        restTemplate = new RestTemplateBuilder()
-                .interceptors((request, body, execution) -> {
-                    logger.info(String.format("Calling %s %s", request.getMethod(), request.getURI()));
-                    ClientHttpResponse clientHttpResponse = execution.execute(request, body);
-                    logger.info(String.format("Call completed %s %s responded with %s", request.getMethod(), request.getURI(), clientHttpResponse.getStatusCode()));
-                    return clientHttpResponse;
+        if(restTemplate==null){
+            // Initialize restTemplate. This is executed only once.
+            restTemplate = new RestTemplateBuilder()
+                    .interceptors((request, body, execution) -> {
+                        logger.info(String.format("Calling %s %s", request.getMethod(), request.getURI()));
+                        ClientHttpResponse clientHttpResponse = execution.execute(request, body);
+                        logger.info(String.format("Call completed %s %s responded with %s", request.getMethod(), request.getURI(), clientHttpResponse.getStatusCode()));
+                        return clientHttpResponse;
                     })
-                .build();
+                    .build();
+        }
+
+
         
         //return restTemplate object
 
